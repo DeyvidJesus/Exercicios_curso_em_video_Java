1bcd6c9084f4f20802554a05fef15ee998425e7c
Deyvid <deyvidgondim@outlook.com>
2022-06-01 20:13:06 -0300

    Exercícios propostos

---
+++ src/Desafios/desafioIdioma.java
@@ -0,0 +1,14 @@
+package Desafios;
+
+import java.util.Locale;
+
+public class desafioIdioma {
+
+	public static void main(String[] args) {
+		Locale location = Locale.getDefault();
+		System.out.print("O idioma do sistema é ");
+		System.out.print(location.getDisplayLanguage() + ".");
+		
+	}
+
+}
